873f5658064ea0a358a336586e5e4af2cb13d2b6
David Boedigheimer <stealth259@gmail.com>
2021-11-29 15:32:38 -0800

    Initial creation of weapon shop.

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <React.Fragment>
+            <h1>Welcome to The Weapon Shop</h1>
+        </React.Fragment>
+    );
+}
+
+export default Header;
